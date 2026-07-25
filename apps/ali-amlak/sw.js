95e7b7bd91ec5759bfaed98dbe6aee0102eb531f
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-07-25 09:00:56 +0300

    Update sw.js

--- apps/ali-amlak/sw.js
+++ apps/ali-amlak/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ali-amlak-v5-1';
+const CACHE='ali-amlak-v6-1';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
