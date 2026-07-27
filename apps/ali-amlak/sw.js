de5a0686c98ab824690d09369f6198468a5f90af
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-07-27 10:00:16 +0300

    Update sw.js

--- apps/ali-amlak/sw.js
+++ apps/ali-amlak/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ali-amlak-v12-print-sizes';
+const CACHE='ali-amlak-v13-print-sizes';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./manifest.json'])).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));return}e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
