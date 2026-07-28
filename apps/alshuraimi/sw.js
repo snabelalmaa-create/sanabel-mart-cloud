3193a4be993c0b9ff0cf8bd11101eb0286ff0b90
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-07-28 22:44:13 +0300

    Update sw.js

--- apps/alshuraimi/sw.js
+++ apps/alshuraimi/sw.js
@@ -1,11 +1,13 @@
-const CACHE_NAME='shuraimi-v120';
+const CACHE_NAME='shuraimi-v110';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
   self.skipWaiting();
 });
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch',event=>{
