8fe3ce22f426204890909a46e1bb9f176935f0fe
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-08-13 19:32:02 +0300

    Create sw.js

---
+++ apps/expiry/sw.js
@@ -0,0 +1,10 @@
+const CACHE='expiry-v5-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ const u=new URL(e.request.url);
+ if(u.hostname.includes('supabase.co')) return;
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+});
