94ba7230f6c0201234a4020e307441f379ca23c3
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-08-18 11:43:14 +0300

    Update sw.js

--- apps/expiry/sw.js
+++ apps/expiry/sw.js
@@ -1,4 +1,4 @@
-const CACHE='expiry-v5-505';
+const CACHE='expiry-v5-507';
 const ASSETS=['./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
