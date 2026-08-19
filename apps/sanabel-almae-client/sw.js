3e144e9c327497d418130133caf297838282abab
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-08-19 22:04:34 +0300

    Add files via upload

---
+++ apps/sanabel-almae-client/sw.js
@@ -0,0 +1,28 @@
+const CACHE='sanabel-almae-client-v10';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k.startsWith('sanabel-almae-client-')&&k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(r=>r||caches.match('./')))
+  );
+});
