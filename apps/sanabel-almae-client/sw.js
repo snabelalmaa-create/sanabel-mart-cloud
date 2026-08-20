90d4bc9e48becefac5b771205640a9444610fad6
snabelalmaa-create <snabel.almaa@hotmail.com>
2026-08-20 13:51:25 +0300

    Add files via upload

--- apps/sanabel-almae-client/sw.js
+++ apps/sanabel-almae-client/sw.js
@@ -1,4 +1,4 @@
-const CACHE='sanabel-almae-client-v15';
+const CACHE='sanabel-almae-client-v16';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./logo-sanabel.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('sanabel-almae-client-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
